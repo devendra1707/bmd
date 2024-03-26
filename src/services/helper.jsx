a2a5723093b0ce50213cb58e465fe0ef5f3ec3fc
devendra1707 <pdevendra033@gmail.com>
2024-03-26 19:02:13 +0530

    update

--- src/services/helper.jsx
+++ src/services/helper.jsx
@@ -3,7 +3,6 @@ import { getToken } from "../auth";
 
 export const BASE_URL = "https://bmdpro.onrender.com/bmd/";
 // export const BASE_URL = "http://localhost:8081/bmd/";
-// export const BASE_URL = "http://localhost:8080/o/c/newaccounts/";
 
 export const myAxios = axios.create({
   baseURL: BASE_URL,
